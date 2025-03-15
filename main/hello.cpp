f451a2686137ceda688475722818d91949f7cbe9
Bindugowda2004 <110481637+Bindugowda2004@users.noreply.github.com>
2025-03-15 13:57:58 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello, BANGALORE\n";
+  cout << "Hello, PESU\n";
   cout << "I have successfully built and run this  \n";
   return 0;
 }
